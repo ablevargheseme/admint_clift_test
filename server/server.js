4e2e6690a965577762b86c8408cb8b92f7b075b6
ablevargheseme <ablevargheseme@gmail.com>
2024-03-14 15:38:28 +0530

    fix:api controller

--- server/server.js
+++ server/server.js
@@ -1,15 +1,18 @@
-const express = require('express');
+import express from 'express';
+ import  update_site  from './services/update_site.js';
 const app = express();
 const port = 3000; // You can choose any available port number
 
 app.get('/', (req, res) => {
     // Extracting query parameters from the request
     const { name} = req.query;
+    const {campaign_id}=req.query;
 
     // Checking if both parameters exist
-    if (name) {
+    if (campaign_id) {
+         update_site(campaign_id,name)
         // Sending the parameters as a response
-        res.send(`name ${name}`);
+        res.send(`campaign site update ${campaign_id}`);
     } else {
         // Sending an error message if parameters are missing
         res.status(400).send('query parameters are required.');
