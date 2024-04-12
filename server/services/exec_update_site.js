4f251e35dd60f3f7be0500ae5278ece02811aaa2
ablevargheseme <ablevargheseme@gmail.com>
2024-04-12 17:57:04 +0530

    test

--- server/services/exec_update_site.js
+++ server/services/exec_update_site.js
@@ -117,6 +117,7 @@ const apiUrl = 'https://campaign.maxz.io/sitedata/trial';
 
 const createTextsFile = async (fileName, finalContent) => {
     await fs.promises.writeFile(fileName, finalContent);
+    console.log("texts file created ")
 };
 const cliftUpload = () => {
     // Change the current working directory
@@ -172,59 +173,25 @@ const update_site = async (campaignId, name) => {
     const finalContent = fileContent + tail_content;
 
     // Define the folder path where the file will be created
-    const folderPath = `../FTD_CODES`; // Replace this with your desired absolute path
+   const folderPath = `../FTD_CODES`; // Replace this with your desired absolute path
 
-    // // Check if the folder exists
-    // if (!fs.existsSync(folderPath)) {
-    //     // Folder does not exist, create it
-    //     try {
-    //         fs.mkdirSync(folderPath, { recursive: true });
-    //         console.log(`Folder '${folderPath}' created successfully.`);
-    //     } catch (err) {
-    //         console.error(`Error creating folder '${folderPath}':`, err);
-    //         process.exit(1); // Exit the script with a non-zero status code
-    //     }
-    // } else {
-    //     console.log(`Folder '${folderPath}' already exists.`);
-    // }
-
-    // // Define the file name with .ftd extension
+  
     const fileName = path.join(folderPath, 'texts.ftd');
 
     // Write the content to the file
     await createTextsFile(fileName, finalContent);
-    cliftUpload();
-
-    // fs.writeFile(fileName, finalContent, (err) => {
-    //     if (err) {
-    //         console.error('An error occurred while writing the file:', err);
-    //     } else {
-    //         console.log(`File '${fileName}' created successfully.`);
-    //         // Change the current working directory
-    //         process.chdir('../FTD_CODES'); // Replace this with your desired absolute path
-    //         const env = Object.assign({}, process.env, {
-    //             FIFTHTRY_SITE_WRITE_TOKEN: 'ab430fa2-1935-48a4-9894-e32f8e8749b5' // Replace this with your token value
-    //         });
-    //         exec('export FIFTHTRY_SITE_WRITE_TOKEN=ab430fa2-1935-48a4-9894-e32f8e8749b5 && clift upload able', (error, stdout, stderr) => {
-    //             if (error) {
-    //                 console.error(`Error executing command: ${error.message}`);
-    //                 return;
-    //             }
-    //             if (stderr) {
-    //                 console.error(`Command stderr: ${stderr}`);
-    //                 return;
-    //             }
-    //             console.log(`Command stdout: ${stdout}`);
-    //         });
-
-
-    //     }
-    // });
-
-
-
-
-
+    process.chdir('../FTD_CODES');
+      exec('export FIFTHTRY_SITE_WRITE_TOKEN=ab430fa2-1935-48a4-9894-e32f8e8749b5 && clift upload able', (error, stdout, stderr) => {
+        if (error) {
+            console.error(`Error executing command: ${error.message}`);
+            return;
+        }
+        if (stderr) {
+            console.error(`Command stderr: ${stderr}`);
+            return;
+        }
+        console.log(`Command stdout: ${stdout}`);
+    });
 
 
 
